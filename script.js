f16b217733abe7c93f1e5dfbb6b4a298fbecf764
Akshay Chaudhari <104254843+akshaychaudhari1499@users.noreply.github.com>
2023-03-09 21:54:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 //your JS code here. If required.
-let imgcont=document.getElementById('img');
-let animals=[camel,
-cheetah,
-elephant,
-gorilla,
-koala,
-lion,
-polar_bear,
-tiger,
-wolf]
-animals.map(animal=>{
-document.write(animal)
+let imgcont= document.getElementById('img');
 
-// url=`https://www2.d125.org/applied_arts/teched/courses/WEB/portfolios/2014_FallP8/Rami_Portfolio_Period8/unit4_css/project_1folder/cssProject1/${animal}.jpg`
-// imgcont.innerHTML+=`<img src=${url}'`	
-
-})
+let animals=['camel','cheetah','elephant','gorilla','koala','lion','polar_bear','tiger','wolf'];
+ animals.map(animal=>{
+// //document.write(animal)
+	url=`https://www2.d125.org/applied_arts/teched/courses/WEB/portfolios/2014_FallP8/Rami_Portfolio_Period8/unit4_css/project_1folder/cssProject1/${animal}.jpg`
+	imgcont.innerHTML +=`<img src=${url}>`
+	
+}
+)
