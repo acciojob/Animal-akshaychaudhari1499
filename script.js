366f6d860981576a0c1f5821a5131ff35900049f
Akshay Chaudhari <104254843+akshaychaudhari1499@users.noreply.github.com>
2023-03-09 21:20:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-container=document.getElementById('container');
+let imgcont=document.getElementById('img');
 let animals=[camel,
 cheetah,
 elephant,
@@ -9,9 +9,10 @@ lion,
 polar_bear,
 tiger,
 wolf]
-animals.forEach(animal=>{
-img=document.createElement('div')
-img.innerHTML=`<img src='https://www2.d125.org/applied_arts/teched/courses/WEB/portfolios/2014_FallP8/Rami_Portfolio_Period8/unit4_css/project_1folder/cssProject1/`${animal}`.jpg' alt='${animal}'`	
-container.appendChild(img)
+animals.map(animal=>{
+document.write(animal)
+
+// url=`https://www2.d125.org/applied_arts/teched/courses/WEB/portfolios/2014_FallP8/Rami_Portfolio_Period8/unit4_css/project_1folder/cssProject1/${animal}.jpg`
+// imgcont.innerHTML+=`<img src=${url}'`	
 
 })
