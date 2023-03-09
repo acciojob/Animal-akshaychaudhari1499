28379253a3c56d4bf48f4d6941c655b9c2cba67a
Akshay Chaudhari <104254843+akshaychaudhari1499@users.noreply.github.com>
2023-03-09 21:56:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let animals=['camel','cheetah','elephant','gorilla','koala','lion','polar_bear',
  animals.map(animal=>{
 // //document.write(animal)
 	url=`https://www2.d125.org/applied_arts/teched/courses/WEB/portfolios/2014_FallP8/Rami_Portfolio_Period8/unit4_css/project_1folder/cssProject1/${animal}.jpg`
-	imgcont.innerHTML +=`<img src=${url}>`
+	imgcont.innerHTML +=`<img src=${url} alt=${animal}>`
 	
 }
 )
